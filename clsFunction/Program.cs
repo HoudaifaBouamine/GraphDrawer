eca3ff039f03cbe90b7d406d139c305c3f7d8653
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-07-26 12:39:34 +0100

    Creating the class

--- clsFunction/Program.cs
+++ clsFunction/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace clsFunction
+namespace nsFunction
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            clsFunction fun = new clsFunction("2 x");
+            Console.WriteLine(fun.calc(2));
+
+            Console.ReadLine();
+        }
+
+        class clsFunction
+        {
+            private string expression;
+            public clsFunction(string expression) 
+            {
+                this.expression = "";
+                for(int i = 0; i < expression.Length;i++)
+                {
+                    if (expression[i] != ' ')
+                    {
+                        this.expression += expression[i];
+                    }
+                }
+            }
+
+            public float calc(float input)
+            {
+
+
+                return 2 * input;
+            }
         }
     }
 }
