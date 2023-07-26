4a507e60215165803975e644dfb2743ab4171497
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-07-26 12:55:37 +0100

    Creating struct clsFunction.stSubstring to replace the calculation with results

--- clsFunction/Program.cs
+++ clsFunction/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +13,11 @@ namespace nsFunction
         static void Main(string[] args)
         {
             clsFunction fun = new clsFunction("2 x");
-            Console.WriteLine(fun.calc(2));
-
+            clsFunction.stSubString sub = new clsFunction.stSubString();
+            sub.open = 3;
+            sub.close = 5;
+            string test = sub.replace("houdaifa", "+-+");
+            Console.WriteLine(test);
             Console.ReadLine();
         }
 
@@ -31,11 +36,38 @@ namespace nsFunction
                 }
             }
 
+            private float calc(string expression,float input)
+            {
+
+
+
+                return input;
+            }
             public float calc(float input)
             {
 
 
-                return 2 * input;
+
+                return input;
+            }
+
+            
+            public struct stSubString
+            {
+                public int start;
+                public int end;
+
+                public int open;
+                public int close;
+
+              
+                public string replace(string source,string new_substring)
+                {
+                    source = source.Remove(open, close - open + 1);
+                    source = source.Insert(open,new_substring);
+
+                    return source;
+                }
             }
         }
     }
