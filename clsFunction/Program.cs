870d604f614e97bad007d317234d2928b9fb59a1
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-07-27 11:36:54 +0100

    Changing the way of calculating function result from string to List of stOpNode that determine if the node is a number or operation or bruckets or util funciton

--- clsFunction/Program.cs
+++ clsFunction/Program.cs
@@ -13,36 +13,21 @@ namespace nsFunction
         static void Main(string[] args)
         {
             clsFunction fun = new clsFunction("2 x");
-            clsFunction.stSubString sub = new clsFunction.stSubString();
-            sub.open = 3;
-            sub.close = 5;
-            string test = sub.replace("houdaifa", "+-+");
-            Console.WriteLine(test);
+           
             Console.ReadLine();
         }
 
         class clsFunction
         {
-            private string expression;
+            private List<stOpNode> expression;
             public clsFunction(string expression) 
             {
-                this.expression = "";
-                for(int i = 0; i < expression.Length;i++)
-                {
-                    if (expression[i] != ' ')
-                    {
-                        this.expression += expression[i];
-                    }
-                }
+                
+
+                
             }
 
-            private float calc(string expression,float input)
-            {
-
-
-
-                return input;
-            }
+           
             public float calc(float input)
             {
 
@@ -51,24 +36,19 @@ namespace nsFunction
                 return input;
             }
 
-            
-            public struct stSubString
+            struct stOpNode
             {
-                public int start;
-                public int end;
-
-                public int open;
-                public int close;
-
-              
-                public string replace(string source,string new_substring)
+                private enum enType
                 {
-                    source = source.Remove(open, close - open + 1);
-                    source = source.Insert(open,new_substring);
-
-                    return source;
+                    eOp,
+                    eNumber,
+                    eOpen,
+                    eClose,
+                    eFunction
                 }
             }
+
+
         }
     }
 }
